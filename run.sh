70d994f1f17567064183e5e2d7169b6ae7b902e0
JEONGWOO LEE <ljwljw8541@uos.ac.kr>
2026-03-25 00:13:28 +0900

    feat: 챕터 5-7 - Hello World, libc, 커널 패닉 구현
    변경 내용 요약:
    - SBI 호출(sbi_call), putchar, printf 구현 (챕터 5-6)
    - PANIC 매크로 구현 (챕터 7)
    - common.c, common.h, kernel.h 추가

--- run.sh
+++ run.sh
@@ -8,7 +8,8 @@ CC=/usr/bin/clang  # Ubuntu 등 환경에 따라 경로 조정: CC=clang
 CFLAGS="-std=c11 -O2 -g3 -Wall -Wextra --target=riscv32-unknown-elf -fuse-ld=lld -fno-stack-protector -ffreestanding -nostdlib"
 
 # 커널 빌드
-$CC $CFLAGS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf kernel.c
+$CC $CFLAGS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf \
+    kernel.c common.c
 
 # QEMU 실행
 $QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot \
